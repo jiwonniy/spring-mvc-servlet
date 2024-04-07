f1e5434cd6cb018fd39f17c8af029d2ee884df2c
jiwonniy <jiwoni0707@gmail.com>
2024-04-07 19:16:49 +0900

    Chore: 접근 제어자 private으로

--- src/main/java/hello/servlet/web/frontcontroller/v3/controller/MemberSaveControllerV3.java
+++ src/main/java/hello/servlet/web/frontcontroller/v3/controller/MemberSaveControllerV3.java
@@ -8,7 +8,7 @@ import hello.servlet.web.frontcontroller.v3.ControllerV3;
 import java.util.Map;
 
 public class MemberSaveControllerV3 implements ControllerV3 {
-    MemberRepository memberRepository = MemberRepository.getInstance();
+    private MemberRepository memberRepository = MemberRepository.getInstance();
 
     @Override
     public ModelView process(Map<String, String> paramMap) {
